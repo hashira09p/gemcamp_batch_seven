5faf4fead5588c66f13022af14bd064976fb31a6
Je Rome <jeromeabelida19@gmail.com>
2024-10-17 17:55:21 +0800

    added: address regions, provinces and districts

---
+++ app/services/ph_location_service.rb
@@ -0,0 +1,41 @@
+class PhLocationService
+  attr_reader :url
+
+  def initialize
+    @url = 'https://psgc.gitlab.io/api'
+  end
+
+  def fetch_regions
+    request = RestClient.get("#{url}/regions/")
+    regions = JSON.parse(request.body)
+    regions.each do |region|
+      address_region = Address::Region.find_or_initialize_by(code: region['code'])
+      address_region.name = region['regionName']
+      address_region.save
+    end
+  end
+
+  def fetch_provinces
+    request = RestClient.get("#{url}/provinces")
+    data = JSON.parse(request.body)
+    data.each do |province|
+      region = Address::Region.find_by_code(province['regionCode'])
+      address_province = Address::Province.find_or_initialize_by(code: province['code'])
+      address_province.name = province['name']
+      address_province.region = region
+      address_province.save
+    end
+  end
+
+  def fetch_districts
+    request = RestClient.get("#{url}/districts/")
+    data = JSON.parse(request.body)
+    data.each do |district|
+      region = Address::Region.find_by(code: district['regionCode'])
+      address_district = Address::Province.find_or_initialize_by(code: district['code'])
+      address_district.name = district['name']
+      address_district.region = region
+      address_district.save
+    end
+  end
+end
