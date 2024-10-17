0700ad185b28920106d4c6382dc3a2beb14c1631
Je Rome <jeromeabelida19@gmail.com>
2024-10-17 18:23:26 +0800

    added: city and barangays

--- app/services/ph_location_service.rb
+++ app/services/ph_location_service.rb
@@ -38,4 +38,31 @@ class PhLocationService
       address_district.save
     end
   end
+
+  def fetch_cities
+    request = RestClient.get("#{url}/cities-municipalities/")
+    data = JSON.parse(request.body)
+    data.each do |city|
+      address_city = Address::City.find_or_initialize_by(code: city['code'])
+      address_city.name = city['name']
+      address_city.province = if city['districtCode']
+                                Address::Province.find_by_code(city['districtCode'])
+                              elsif city['provinceCode']
+                                Address::Province.find_by_code(city['provinceCode'])
+                              end
+      address_city.save
+    end
+  end
+
+  def fetch_barangays
+    request = RestClient.get("#{url}/barangays/")
+    data = JSON.parse(request.body)
+    data.each do |barangay|
+      city_code = barangay['cityCode'] ? barangay['cityCode'] : barangay['municipalityCode']
+      address_barangay = Address::Barangay.find_or_initialize_by(code: barangay['code'])
+      address_barangay.name = barangay['name']
+      address_barangay.city = Address::City.find_by(code: city_code)
+      address_barangay.save
+    end
+  end
 end
